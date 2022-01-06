6e33bec8c19c07c78e2d9c234da28de5ce72ba0e
diegoaiapp <diego.gomez.bezmalinovic@gmail.com>
2022-01-06 01:46:13 -0300

    aa

--- db/db.go
+++ db/db.go
@@ -162,7 +162,8 @@ func GetFiltroByteContent(db *sql.DB, id int64) ([]byte, error) {
 	return filtro, nil
 }
 func GetFiltroStringContent(db *sql.DB, id int64) (string, error) {
-	rows, err := db.Query("SELECT filtro FROM db2.filtros WHERE id=?", id)
+
+	rows, err := db.Query("SELECT filtro FROM db1.filtros WHERE id=?", id)
 	if err != nil {
 		return "", err
 	}
